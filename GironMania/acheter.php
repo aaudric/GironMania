e00a2259099325bf68bda0c05d8a7ca7808fdad3
aaudric <audricgir@gmail.com>
2023-04-15 14:15:40 +0200

    petite rectification de sécurité

--- GironMania/acheter.php
+++ GironMania/acheter.php
@@ -23,7 +23,7 @@
     <br/>
     <br/>
 
-    <h2 class ="achat">Félicitations</h2>
+    
 
 
     <?php
@@ -34,31 +34,40 @@
         
         $bdd= getBD();
 
-        // Boucle pour chaque article du panier
-        foreach($_SESSION['panier'] as $article) {
+        if (isset($_SESSION['panier']) and !empty($_SESSION['panier'])){
 
-            // Récupérer les informations de l'article
-            $id_art = $article[0];
-            $id_client = $_SESSION['client']['id_client'];
-            $quantite = $article[1];
+            // Boucle pour chaque article du panier
+            foreach($_SESSION['panier'] as $article) {
 
-            // Insérer l'article dans la table Commandes
-            $q = $bdd -> prepare("INSERT INTO `commandes` (`id_art`, `id_client`, `quantite`, `envoi`) VALUES ($id_art, $id_client, $quantite, '0')");
+                // Récupérer les informations de l'article
+                $id_art = $article[0];
+                $id_client = $_SESSION['client']['id_client'];
+                $quantite = $article[1];
+
+                // Insérer l'article dans la table Commandes
+                $q = $bdd -> prepare("INSERT INTO `commandes` (`id_art`, `id_client`, `quantite`, `envoi`) VALUES ($id_art, $id_client, $quantite, '0')");
+                
+                $q->execute(array('id_art'=>$id_art, 'id_client'=>$id_client, 'quantite'=>$quantite, 'envoi'=>0));
+
+                // Mettre à jour la quantité en stock de l'article
+                $upd = $bdd -> prepare("UPDATE Articles SET quantite = quantite - $quantite WHERE `articles`.`id_art` = $id_art");
+
+                $update = $upd ->execute();
+
+            }
+
+            echo '<h2 class ="achat">Félicitations</h2>'; 
+
+            echo "<p class ='large'> Votre achat a été effectué avec succès et votre commande a bien été enregistrée.</p>";
+
+            // Supprime la variable de session contenant le panier
+            unset($_SESSION['panier']);
             
-            $q->execute(array('id_art'=>$id_art, 'id_client'=>$id_client, 'quantite'=>$quantite, 'envoi'=>0));
+        }else{
 
-            // Mettre à jour la quantité en stock de l'article
-            $upd = $bdd -> prepare("UPDATE Articles SET quantite = quantite - $quantite WHERE `articles`.`id_art` = $id_art");
-
-            $update = $upd ->execute();
-
+            echo "<h2 class ='achat'>Désolé</h2>
+            <p class ='large'>Votre panier est vide, il y a surement eu une erreur, veuillez réesayer.</p>";  
         }
-
-        echo "<p class ='large'> Votre achat a été effectué avec succès et votre commande a bien été enregistrée.</p>";
-
-        // Supprime la variable de session contenant le panier
-        unset($_SESSION['panier']);
-
     ?>
 
     <footer>
